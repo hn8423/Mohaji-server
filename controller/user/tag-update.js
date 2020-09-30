5d1ee524165a1d3327b1a0203f2b696177b00bd0
Jiwook00 <jwkim775@gmail.com>
2020-09-30 22:21:12 +0900

    마이페이지 유저 태그 변경
    tag-update에서 유저가 태그를 변경할 수 있게 제작함

---
+++ controller/user/tag-update.js
@@ -0,0 +1,21 @@
+const { user } = require('../../models');
+const { user_tag } = require('../../models');
+
+module.exports = {
+    post: async (req, res) => {
+        let { userid } = req.session;
+        let { tag } = req.body;
+        if (userid) {
+            let userData = await user.findOne({ where: { id: userid.id } });
+            user_tag.destroy({ where: { user_id: userData.id } });
+            for (let i in tag) {
+                if (tag[i]) {
+                    await user_tag.create({ user_id: userData.id, tag_id: i });
+                };
+            };
+            return res.status(201).send('수정 완료')
+        } else {
+            return res.status(401).send('session not fonud')
+        }
+    }
+};
