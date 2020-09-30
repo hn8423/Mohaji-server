8fbd71b2c9c0bb2a0ed65290fc5aba08276ab282
Jiwook00 <jwkim775@gmail.com>
2020-09-30 18:45:31 +0900

    signup-tag와 user-info
    signup에서req 받은 tag 가저오고 DB에 저장할 수 있도록 수정
    user/info를 이용해서 마이페이지에서 유저가 선택한 tag 리스트도 받을수 있도록 추가

--- controller/user/info.js
+++ controller/user/info.js
@@ -3,58 +3,36 @@ const { user_tag } = require('../../models');
 const { tag } = require('../../models')
 
 module.exports = {
-    // get: async (req, res) => {
-    //     let { userid } = req.session
-    //     if (userid) {
-    //         let data = await user.findOne({ where: { id: userid.id } });
-    //         res.status(200).json(data.dataValues);
-    //     } else {
-    //         res.status(404).send('session not fonud');
-    //     }
-    // }
-
-    // get: async (req, res) => {
-    //     let { userid } = req.session;
-    //     if (userid) {
-    //         let data = await user.findOne({
-    //             where: { id: userid.id },
-    //             include: user_tag
-    //         });
-    //         console.log(data)
-    //         res.status(200).json(data.dataValues);
-    //     } else {
-    //         res.status(404).send('session not fonud');
-    //     }
-    // }
     get: async (req, res) => {
         let { userid } = req.session;
         if (userid) {
-            user.belongsToMany()
             let data = await user.findOne({
                 where: { id: userid.id },
                 include: [
                     {
                         model: user_tag,
-                        include: [user, tag]
                     },
+
                     {
-                        model: tag,
+                        model: user_tag,
                         include: {
-                            model: user,
-                            include: {
-                                model: user_tag,
-                                include: [user, tag]
-                            }
+                            model: tag,
+
                         }
                     }
                 ]
             })
-            console.log("조인 데이터 : ", data)
-            res.status(200).json(data);
+
+            let tagOfUser = data.user_tags.map((val) => val.tag_id)
+            let userInfo = {
+                email: data.email,
+                nickname: data.nickname,
+                tag: tagOfUser
+            }
+            res.status(200).json(userInfo);
         } else {
             res.status(404).send('session not fonud');
         }
     }
 
 };
-
