1097a5673087586bcd8dd6971f08aea08251af86
김현인 <rolenche@gmail.com>
2020-10-04 21:31:03 +0900

    a

--- controller/user/socialSignup.js
+++ controller/user/socialSignup.js
@@ -1,17 +1,17 @@
 const axios = require('axios');
-const { user:User, user_tag:User_tag } = require('../../models');
+const { user: User, user_tag: User_tag } = require('../../models');
 require('dotenv').config();
 
 module.exports = {
   post: async (req, res) => {
     let { token, nickname, tag } = req.body;
-    let { data:result, status } = await axios.get(`https://oauth2.googleapis.com/tokeninfo?id_token=${token}`).catch(err => err.response);
+    let { data: result, status } = await axios.get(`https://oauth2.googleapis.com/tokeninfo?id_token=${token}`).catch(err => err.response);
     if (status === 200 && result.azp === process.env.google_client_id && nickname && tag) {
       let user = await User.create({
         nickname,
         social: true,
         social_id: result.sub
-      }).catch(err=>console.log(err));
+      }).catch(err => console.log(err));
       console.log(result.sub)
       if (user) {
         for (let i in tag) {
